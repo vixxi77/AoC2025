91fb32d3e7a0fe84456bb532cde1fec3f6fa539e
yaye <viktorastankus37@gmail.com>
2025-12-02 19:46:24 +0200

    init

---
+++ day1/main.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MIN 0
+#define MAX 99
+
+int main(){
+	FILE *file;
+	file = fopen("input.txt", "r");
+	char buffer[10];
+	int finished_flag = 0;
+	char direction;
+	int zero_count = 0;
+	int code_position = 50;
+	
+	while(fgets(buffer, 10, file)){
+		int number = 0;
+		for(int i = 0; buffer[i] != '\n'; i++){
+			if(buffer[i] == 'R' || buffer[i] == 'L'){
+				direction = buffer[i];
+			}else{
+				char c = buffer[i];
+				number = number * 10 + atoi(&c);
+			}
+		}
+
+		switch(direction){
+		case 'R':
+			for(int i = 1; i != (number + 1); i++){
+				if(code_position == MAX){
+					code_position = -1;
+				}
+				code_position += 1;	
+			}
+			break;
+		case 'L':
+			for(int i = 1; i != (number + 1); i++){
+				if(code_position == MIN){
+					code_position = 100;
+				}
+				code_position -= 1;	
+			}
+			break;
+		}
+		if(code_position == 0)
+			zero_count++;
+
+	}
+
+	printf("Amount of times the arrow was left on zero: %d \n", zero_count);
+
+
+	fclose(file);
+	return 0;
+}
+
